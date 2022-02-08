b21efaa6c98c3b9801b1afac4756f444a6726524
bearbaba <2200522850@qq.com>
2022-02-08 23:41:19 +0800

    test: 使用prettyDOM打印组件内容

--- src/components/note.test.js
+++ src/components/note.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { prettyDOM, render } from "@testing-library/react";
 import Note from "./note";
 
 test("renders content", () => {
@@ -11,6 +11,18 @@ test("renders content", () => {
   const component = render(<Note content={note.content} />);
 
   expect(component.container).toHaveTextContent(
+    "Component testing is done with testing-library/react"
+  );
+
+  const element = component.getByText(
+    "Component testing is done with testing-library/react"
+  );
+
+  expect(element).toBeDefined();
+
+  const div = component.container.querySelector(".note");
+  console.log(prettyDOM(div));
+  expect(div).toHaveTextContent(
     "Component testing is done with testing-library/react"
   );
 });
